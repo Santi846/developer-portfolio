bdbd48cb79b79fd68efbc17ce00b4f759bd91002
Santiago Arocha <arochas846@gmail.com>
2024-04-04 00:13:54 -0300

    Setting up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css';
+import NavbarComponent from './components/NavBar';
+
+function App() {
+
+  return (
+    <>
+      <NavbarComponent></NavbarComponent>
+    </>
+  )
+}
+
+export default App
